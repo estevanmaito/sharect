f8953bc0fe1ddb49c86e35f30024226de6000a73
Estevan Maito <ejmaito@gmail.com>
2019-05-18 16:53:24 -0300

    add IE fallback

--- src/helpers/isSelectableElement.js
+++ src/helpers/isSelectableElement.js
@@ -1,7 +1,8 @@
 import getClosestElement from "./getClosestElement";
 
 export default function isSelectableElement(selectableElements) {
-  let currentSelectedElement = window.getSelection().baseNode.parentNode
+  const baseNode = window.getSelection().baseNode || window.getSelection().anchorNode
+  let currentSelectedElement = baseNode.parentNode
   return selectableElements.some(ancestor =>
     getClosestElement(currentSelectedElement, ancestor)
   )
